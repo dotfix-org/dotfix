c5f193db8b071367f91b868e0b82d56651f7b4f6
Molen <prem20032009@gmail.com>
2024-12-08 11:40:32 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SignupComponent} from './signup/signup.component';
+import {HomeComponent} from './home/home.component';
+import {BookingComponent} from './booking/booking.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, SignupComponent, HomeComponent, BookingComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {RouterModule,Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {HomeComponent} from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {MonitorComponent} from './monitor/monitor.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'home' ,component:HomeComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'booking', component: BookingComponent },
+  {path:'monitor',component:MonitorComponent},
+  { path: '', redirectTo: '/monitor', pathMatch: 'full' },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),BrowserModule, FormsModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
+
